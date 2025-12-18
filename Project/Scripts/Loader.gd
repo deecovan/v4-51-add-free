7f57f23a303ed5f541320399af7fad6869c49078
Dee <dee.covan@gmail.com>
2025-12-18 02:15:10 +0200

    Add Menus and Buttons test sequences

--- Project/Scripts/Loader.gd
+++ Project/Scripts/Loader.gd
@@ -1,7 +1,20 @@
 extends Node2D
 
+@export var button: Button
+@export var title: Label
 @export var game_menu: Panel
 @export var drive_menu: Panel
+@export var circle_menu: Panel
+var menus: Array
+var panels: Array
+
+func _ready() -> void:
+	panels = get_tree().get_nodes_in_group("panels")
+	menus = get_tree().get_nodes_in_group("menus")
+	hide_menus()
+	show_panels()
+	button.hide()
+	game_menu.show()
 
 func _process(_delta):
 	if Input.is_action_just_pressed('reload'):
@@ -13,25 +26,53 @@ func _process(_delta):
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN \
 			if is_window else DisplayServer.WINDOW_MODE_WINDOWED)
 
-func _ready() -> void:
-	hide_menus()
-	game_menu.show()
-	
-func hide_menus() -> void:
-	var menus = get_tree().get_nodes_in_group("menus")
-	for menu in menus:
-		menu.hide()
-	
-func _on_play_game_btn_pressed() -> void:
-	game_menu.hide()
-	drive_menu.show()
-	
-func load_scene() -> void:
-	pass
-	
-func _on_back_btn_pressed() -> void:
-	hide_menus()
-	game_menu.show()
+func _on_button_pressed() -> void:
+	print ("<<PLAYING>>")
+	_on_home_btn_pressed()
 
+func _on_home_btn_pressed() -> void:
+	hide_menus()
+	show_panels()
+	button.hide()
+	game_menu.show()
+	
 func _on_quit_btn_pressed() -> void:
 	get_tree().call_deferred("quit")
+	
+func hide_menus() -> void:
+	for menu in menus:
+		menu.hide()
+
+func hide_panels() -> void:
+	for panel in panels:
+		panel.hide()
+		
+func show_panels() -> void:
+	for panel in panels:
+		panel.show()
+	
+func _on_play_game_btn_pressed() -> void:
+	hide_menus()
+	hide_panels()
+	drive_menu.show()
+
+func _on_circle_tracks_pressed() -> void:
+	hide_menus()
+	hide_panels()
+	circle_menu.show()
+
+func _on_square_land_pressed() -> void:
+	hide_menus()
+	hide_panels()
+	button.show()
+
+func _on_spa_flat_pressed() -> void:
+	hide_menus()
+	hide_panels()
+	button.show()
+
+
+func _on_cup_circus_pressed() -> void:
+	hide_menus()
+	hide_panels()
+	button.show()
