5dba08a3a77e271de7575bc03151305832e8f006
Dee <dee.covan@gmail.com>
2026-01-30 16:17:40 +0200

    Up: Test and improve Light

--- Project/Scenes/Sky/timer.gd
+++ Project/Scenes/Sky/timer.gd
@@ -5,22 +5,36 @@ var step = 0.01
 var speed = 0.05
 var sky: WorldEnvironment
 var phase = -1
+var p = 1.0
+var intensity = 9200
 
 func _ready() -> void:
 	sky = get_parent()
 	wait_time = speed
 	autostart = true
 	start()
-	sky.environment.adjustment_brightness = 1
-	sky.environment.background_energy_multiplier = 1
-	sky.environment.background_intensity = 32000
+	sky.environment.adjustment_brightness = p
+	sky.environment.background_energy_multiplier = p
+	sky.environment.background_intensity = p * intensity
 
 func _on_timeout() -> void:
-	$"../DirectionalLight3D".rotate_x(step)
-	sky.environment.adjustment_brightness += phase * step
-	sky.environment.background_energy_multiplier += phase * step
-	sky.environment.background_intensity += phase * step * 32000
+	p += phase * step
+	if sky.environment.adjustment_brightness < step:
+		sky.environment.adjustment_brightness = step
+	if sky.environment.background_energy_multiplier < step:
+		sky.environment.background_energy_multiplier = step
+	if sky.environment.background_intensity < 1:
+		sky.environment.background_intensity = 1
+	sky.environment.adjustment_brightness = p
+	sky.environment.background_energy_multiplier = p
+	sky.environment.background_intensity = p * intensity
 	if (sky.environment.adjustment_brightness <= step
 		or sky.environment.adjustment_brightness >= 1.0):
 		phase = -phase
+	printt(
+		phase, p,
+		sky.environment.adjustment_brightness,
+		sky.environment.background_energy_multiplier,
+		sky.environment.background_intensity
+	)
 	
